20978eb9d4368b1aa4db0454b38ee4c2a04a4a16
Adrian Kenepah <ackenepah@gmail.com>
2017-08-15 19:11:48 -0700

    Blog Posts 1.) Installed Axios 2.) Using Axios to pull data from a json placeholder site until we get the database created for actual blog posts 3.) Getting data to display on DOM and just need to style it.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Main extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      posts : []
+    }
+  }
+
+  componentDidMount() {
+    console.log("WORKING!")
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      const posts = res.data.map((item, index) => {
+        return item;
+      })
+
+      this.setState({ posts : posts });
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <p>MAIN</p>
+        { this.state.posts.map((post, key) => {
+          return (
+            <div key={key}>
+              <h1>{ post.title }</h1>
+              <p>{ post.body }</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
