f57b868abdda59d6e59fb9496205303c36caee68
Adrian Kenepah <ackenepah@gmail.com>
2017-08-29 14:20:23 -0700

    Admin Page 1.) Got admin page structured out. 2.) Added button to link back to home.

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../css/admin.css';
 
 class Admin extends Component {
 
   render() {
     return (
       <div className="App">
-        <p>ADMIN</p>
+        <div className="admin-header">
+
+        </div>
+
+        <form>
+          <h2>Post Title</h2>
+          <p>Lorem</p>
+          <input type="text" />
+
+          <h2>Category</h2>
+          <p>Lorem</p>
+          <input id="news" type="checkbox" /><label htmlFor="news">News</label>
+          <input id="weekly-recaps" type="checkbox" /><label htmlFor="weekly-recaps">Weekly Recaps</label>
+          <input id="draft" type="checkbox" /><label htmlFor="draft">Draft</label>
+          <input id="playoffs" type="checkbox" /><label htmlFor="playoffs">Playoffs</label>
+          <input id="rules" type="checkbox" /><label htmlFor="rules">Rules</label>
+
+          <h2>Post Body</h2>
+          <p>Lorem</p>
+          <textarea>
+          </textarea>
+
+          <h2>Image URL</h2>
+          <p>Lorem</p>
+          <input type="text" />
+
+          <button type="submit">SUBMIT</button>
+        </form>
+
+        <Link to="/"><p>HOME</p></Link>
       </div>
     );
   }
